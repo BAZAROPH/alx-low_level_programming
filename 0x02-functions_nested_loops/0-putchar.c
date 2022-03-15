4f4f6423523a1a551a9f7f9d98b9e43981b164e8
Coulibaly Cheick Oumar <bazaroph@gmail.com>
2022-03-15 13:00:33 +0000

    header/h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <main.h>
 
 /* This function prints _putchar, followed by a new line */
 
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* function print character */
+
+void _putchar(char caracter){
+	print("%c", caracter);
+}
