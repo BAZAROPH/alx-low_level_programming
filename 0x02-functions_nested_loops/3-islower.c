112f0b5adb0af7c98d575c1f85c6bd0f7dadca37
Coulibaly Cheick Oumar <bazaroph@gmail.com>
2022-03-15 15:13:19 +0000

    FYI: The standard library provides a similar function: islower. Run man islower to learn more.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower - return 0 if arg is not lower else it return 1,
+* follwed by a new line
+*/
+int _islower(int caracter)
+{
+	if (caracter >=97 && caracter <=122 )
+	{
+		return (1);
+	}else{
+		return (0);
+	}
+} 
