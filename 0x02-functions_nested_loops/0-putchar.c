ee54325c50436d7a3c523fdf272c78a9ffcbeb37
Coulibaly Cheick Oumar <bazaroph@gmail.com>
2022-03-15 13:05:23 +0000

    header/h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,14 @@
 /* This function prints _putchar, followed by a new line */
 
 int main(){
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	printf("\n");
 	return 0;
 }
