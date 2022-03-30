b0d1687933649cf2cea8f08a1e24f0c6217a643d
Coulibaly Cheick Oumar <bazaroph@gmail.com>
2022-03-30 09:34:44 +0000

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+* _puts_recursion: prints a string, followed by a new line.
+* @s: is a string to print
+*/
+
+void _puts_recursion(char *s)
+{
+	printf("%c", s[0]);
+	int i = 0;
+	int size = strlen(s);
+
+	if (size > 0)
+	{
+		int a = 0;
+		char d[size - 1];
+
+		for (i = 1 ; i <= size; i++)
+		{
+			d[a] = s[i];
+			a++;
+		}
+		_puts_recursion(d);
+	}
+	if (size == 0)
+	{
+		printf("\n");
+	}
+}
