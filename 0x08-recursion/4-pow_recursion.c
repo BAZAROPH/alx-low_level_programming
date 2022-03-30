12bfeb24486adaff2e41295bdc9493139399c347
Coulibaly Cheick Oumar <bazaroph@gmail.com>
2022-03-30 10:00:07 +0000

    correction of 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,17 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#include "main.h"
 
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-
-#endif
+/**
+* _pow_recursion - returns the value of x raised to the power of y
+* @x: value to raise
+* @y: power
+*
+* Return: result of the power
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
