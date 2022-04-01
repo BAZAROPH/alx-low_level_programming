9a344f173ca33cb71fc05713f3164bcc3760dde8
Coulibaly Cheick Oumar <bazaroph@gmail.com>
2022-04-01 15:01:08 +0000

    My name is program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - return the name of the program
+* @argc: is parameters number
+* @argv: is array that contains parameters
+* Return: 0 if preogram is good
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc ;
+	printf("%s", argv[0]);
+
+	return (0);
+}
